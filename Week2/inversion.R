3cfefff49bb27887504ed19eb6def5187fda56f0
Michael Salter-Townshend <michael.salter-townshend@ucd.ie>
2022-09-14 14:40:49 +0100

    inversion added

--- Week2/inversion.R
+++ Week2/inversion.R
@@ -5,4 +5,12 @@ inversion = function(n,inv.func) {
 
 inv.exp = function(u,lambda=1){
   -(log(1-u))/lambda 
-  }
+}
+
+inv.max.exp=function(u,m,lambda=1){
+  -1/lambda*log(1-u^(1/m))
+}
+
+inv.min.exp=function(u,m,lambda=1){
+  -log(1-u)/(m*lambda)
+}
