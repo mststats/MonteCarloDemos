53c732c138cc1e2a3f2745a18304d6f5729f88b3
Michael Salter-Townshend <michael.salter-townshend@ucd.ie>
2022-11-07 13:57:03 +0000

    week 8

--- Week8/Metropolis.R
+++ Week8/Metropolis.R
@@ -1,27 +1,37 @@
-iter=1e4
-x=c(3,5,2,3,6,1,0,1,0,1) # sum(x)=22
-n=length(x)
-
-# Metropolis Algorithm
-lambda=rep(NA,iter)
-lambda[1]=0
-prop.sd=1.5
-prop.var=3*(prop.sd^2)
-acc.count=0
-pb=txtProgressBar(min=1,max=iter)
-print("Metropolis:")
-for(i in 2:iter) {
-  setTxtProgressBar(pb, i)
-  lambdastar=rnorm(1,lambda[i-1],prop.sd)
-  r=exp(-n*exp(lambdastar)+sum(x)*lambdastar-0.25*lambdastar^2) /  exp(-n*exp(lambda[i-1])+sum(x)*lambda[i-1]-0.25*lambda[i-1]^2)
-  #logr=(-n*exp(lambdastar)+sum(x)*lambdastar-0.25*lambdastar^2) - (-n*exp(lambda[i-1])+sum(x)*lambda[i-1]-0.25*lambda[i-1]^2)
-  U=runif(1)
-  if(U<r) {
-    lambda[i]=lambdastar 
-    acc.count=acc.count+1
-  } else {
-    lambda[i]=lambda[i-1] 
+# Metropolis Algorithm for Poisson lambda=log(rate) given counts vector x
+Metropolis=function(x,init,iter) {
+  n=length(x)
+  lambda=rep(NA,iter)
+  lambda[1]=init
+  prop.sd=1.5
+  acc.count=0
+  q=function(t.lambda)
+    exp(-n*exp(t.lambda)+sum(x)*t.lambda-0.25*t.lambda^2) 
+  pb=txtProgressBar(min=1,max=iter)
+  print("Metropolis:")
+  for(i in 2:iter) {
+    setTxtProgressBar(pb, i)
+    lambdastar=rnorm(1,lambda[i-1],prop.sd)
+    r=q(lambdastar) / q(lambda[i-1])
+    # it can be more stable to use the log of the acceptance ratio:
+    #logr=(-n*exp(lambdastar)+sum(x)*lambdastar-0.25*lambdastar^2) - (-n*exp(lambda[i-1])+sum(x)*lambda[i-1]-0.25*lambda[i-1]^2)
+    # we'd then need to check if log(U)<logr
+    U=runif(1)
+    if(U<r) {
+      lambda[i]=lambdastar 
+      acc.count=acc.count+1
+    } else {
+      lambda[i]=lambda[i-1] 
+    }
   }
+  close(pb)
+  cat("accepted ", signif(100*acc.count/iter,2), "%\n",sep="")
+  return(lambda)
 }
-close(pb)
-cat("accepted ", signif(100*acc.count/iter,2), "%\n",sep="")
+x=c(3,5,2,3,6,1,0,1,0,1) # sum(x)=22
+lambda=Metropolis(x,0,3e4) # one chain of samples
+# create lists of mcmc objects; each object is a Markov Chain of samples
+# purposefully start each chain at a different initial value; e.g. log(chain index)
+require(coda) # to change each chain to an mcmc class object for use with gelman.plot and gelman.diag
+lambda.chains=lapply(1:4, function(i) as.mcmc(Metropolis(x,log(i),1e4))) 
+
--- Week8/MetropolisHastings.R
+++ Week8/MetropolisHastings.R
@@ -1,26 +1,37 @@
-iter=1e4
-x=c(3,5,2,3,6,1,0,1,0,1) # sum(x)=22
-n=length(x)
-
-###########
-# Metropolis-Hastings Algorithm
-gammat=rep(NA,iter)
-gammat[1]=1
-acc.count=0
-pb=txtProgressBar(min=1,max=iter)
-print("Metropolis-Hastings:")
-for(i in 2:iter) {
-  setTxtProgressBar(pb, i)
-  gammastar=rgamma(1,(gammat[i-1]^2)/prop.var,rate=gammat[i-1]/prop.var)
-  r=( ( gammastar^(sum(x)-1) * exp(-n*gammastar - 0.25*log(gammastar)^2) ) / dgamma(gammastar,(gammat[i-1]^2)/prop.var,rate=gammat[i-1]/prop.var) ) /
-       ( ( gammat[i-1]^(sum(x)-1) * exp(-n*gammat[i-1] - 0.25*log(gammat[i-1])^2) ) / dgamma(gammat[i-1],(gammastar^2)/prop.var,rate=gammastar/prop.var) )
-  U=runif(1)
-  if(U<r) {
-    gammat[i]=gammastar 
-    acc.count=acc.count+1
-  } else {
-    gammat[i]=gammat[i-1] 
+# Metropolis-Hastings Algorithm for Poisson rate gamma given counts data x
+MH=function(x, init, iter) {
+  n=length(x)
+  prop.sd=1.5
+  prop.var=3*(prop.sd^2)
+  q=function(t.gamma) 
+    t.gamma^(sum(x)-1) * exp(-n*t.gamma - 0.25*log(t.gamma)^2) 
+  j=function(p.gamma,c.gamma)
+    dgamma(p.gamma,(c.gamma^2)/propt.var,rate=c.gamma/prop.var)
+  gammat=rep(NA,iter)
+  gammat[1]=init
+  acc.count=0
+  pb=txtProgressBar(min=1,max=iter)
+  print("Metropolis-Hastings:")
+  for(i in 2:iter) {
+    setTxtProgressBar(pb, i)
+    gammastar=rgamma(1,(gammat[i-1]^2)/prop.var,rate=gammat[i-1]/prop.var)
+    r=(q(gammastar)/j(gammastar,gammat[i-1])) / (q(gammat[i-1])/j(gammat[i-1],gammastar)) 
+    U=runif(1)
+    if(U<r) {
+      gammat[i]=gammastar 
+      acc.count=acc.count+1
+    } else {
+      gammat[i]=gammat[i-1] 
+    }
   }
+  close(pb)
+  cat("accepted ", signif(100*acc.count/iter,2), "%\n",sep="")
+  return(gammat)
 }
-close(pb)
-cat("accepted ", signif(100*acc.count/iter,2), "%\n",sep="")
+
+x=c(3,5,2,3,6,1,0,1,0,1) # sum(x)=22
+gammat=MH(x,1,3e4) # one chain of samples
+# create lists of mcmc objects; each object is a Markov Chain of samples
+# purposefully start each chain at a different initial value; e.g. chain index
+require(coda) # to change each chain to an mcmc class object for use with gelman.plot and gelman.diag
+gammat.chains=lapply(1:4, function(i) as.mcmc(MH(x,i,1e4)))
